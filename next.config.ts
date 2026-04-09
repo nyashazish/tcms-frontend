561767e69f78a36bbf9adef82d78aaf918c777c2
dave-budah <davebudah@gmail.com>
2026-04-09 07:30:40 +0200

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  devIndicators: false,
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
